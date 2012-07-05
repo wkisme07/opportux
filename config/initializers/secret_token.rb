6220fb000963e961c3209027e78e6b6ebd5647f1
Wawan Kurniawan <ones07@gmail.com>
2012-07-05 10:37:53 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opportux::Application.config.secret_token = '950bebf47af4d3827221746d86190d22afdb2bbb1a2f93afd1dc08a4afd3dc812d3a64c944f12710bafa8957f3fa2e26471b622aa350441a793b34caeda1bb83'
