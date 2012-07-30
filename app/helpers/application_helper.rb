811d6b198c7de2878a8f3fce9ba1c91c696a9d8e
Wawan Kurniawan <ones07@gmail.com>
2012-07-30 13:55:23 +0700

    try to implement facebook like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
 
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => 'add-image')
   end
+
+  def facebook_like
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
 end
