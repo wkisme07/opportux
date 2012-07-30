11da903f7908232dc3bd9a98d8b9e98c5a0409e8
Wawan Kurniawan <ones07@gmail.com>
2012-07-30 11:48:59 +0700

    try implement login with facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,9 +7,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
   end
-  
+
 end
