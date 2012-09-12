d965a2f3bc056bc8a48df35afe33e5f3560c7d8d
Wawan Kurniawan <ones07@gmail.com>
2012-09-12 09:10:03 +0700

    mailer

---
+++ app/mailers/app_mailer.rb
@@ -0,0 +1,21 @@
+class AppMailer < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
+  default from: "no-reply@opportux.com"
+
+  def send_advertise_invoice(adv_id, viewed, amount)
+    prepare_advertise(adv_id)
+    @viewed = viewed
+    @amount = amount
+    mail(
+      to: @adv.contact_email,
+      subject: "[opportux] Advertise Invoice (#{@today.strftime('%Y-%m')})"
+    )
+  end
+
+  private
+
+    def prepare_advertise(adv_id)
+      @today = Date.today
+      @adv = Advertise.find(adv_id)
+    end
+end
